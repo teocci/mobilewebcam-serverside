df2311a2dd974cb8bfaf9de9daea5b2307e0109a
dngames@gmail.com <dngames@gmail.com@ec0c459f-0f12-ddc1-5605-7ea8fad4b955>
2012-03-15 07:44:21 +0000

    create missing archive dir

--- trunk/cam.php
+++ trunk/cam.php
@@ -5,6 +5,8 @@ if(strlen(basename($_FILES["userfile"]["name"])) > 0)
 {
   $uploadfile = basename($_FILES["userfile"]["name"]);
   
+  if(!file_exists("archive/"))
+    mkdir("archive", 0755);
   copy("current.jpg", "./archive/".filemtime("current.jpg").".jpg");
 
   if(move_uploaded_file($_FILES["userfile"]["tmp_name"], $uploadfile))
