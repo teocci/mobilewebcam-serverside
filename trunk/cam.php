6596091085cd19f857ad58ccf397d3395e952151
dngames@gmail.com <dngames@gmail.com@ec0c459f-0f12-ddc1-5605-7ea8fad4b955>
2012-03-15 07:36:13 +0000

    archive version

---
+++ trunk/cam.php
@@ -0,0 +1,20 @@
+<?php
+// new file uploaded?
+$uploadfile = "";
+if(strlen(basename($_FILES["userfile"]["name"])) > 0)
+{
+  $uploadfile = basename($_FILES["userfile"]["name"]);
+  
+  copy("current.jpg", "./archive/".filemtime("current.jpg").".jpg");
+
+  if(move_uploaded_file($_FILES["userfile"]["tmp_name"], $uploadfile))
+  {
+    @chmod($uploadfile,0755);
+    echo "Ok!";
+  }
+  else
+    echo "Error copying!";
+}
+else
+  echo $_POST["userfile"]; // some plain text!
+?>
